5398c4b2d6b51a4766cb0b12ce1ba26826687d78
tamanazehra <zehratamana3@gmail.com>
2025-05-06 17:00:32 +0530

    Count-the-Number

---
+++ Count-The-Number-In-Integer/CSharpProject/TestProject/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter the Number:" );
+int input = Convert.ToInt32(Console.ReadLine());
+
+int newInput;
+
+if (input == 0)
+{
+    Console.WriteLine(1);
+}
+else
+{
+    newInput = Math.Abs(input);
+    int counter = 0;
+
+    while (newInput > 0)
+    {
+        newInput = newInput / 10;
+        counter++;
+    }
+
+    Console.WriteLine("Number of digits: " + counter);
+}
+Console.ReadLine();
