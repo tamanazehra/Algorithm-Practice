57ea0c6690ab6e899e718692d6953d6760f72db8
tamanazehra <zehratamana3@gmail.com>
2025-05-06 17:25:21 +0530

    Counting-Even-Digits

---
+++ Count-Even-Digits/Count-Even-Digits/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter a number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input == 0)
+{
+    Console.WriteLine("The number has 1 even digit.");
+}
+else
+{
+    input = Math.Abs(input);
+    int count = 0;
+
+    while (input > 0)
+    {
+        int digit = input % 10;
+        if (digit % 2 == 0)
+        {
+            count++;
+        }
+        input /= 10;
+    }
+    Console.WriteLine($"The number has {count} even digits.");
+}
+
+
