ba8e942240497d4fb97c277d93aaeaf622d982b7
tamanazehra <zehratamana3@gmail.com>
2025-05-08 16:19:27 +0530

    Count-the-digits

---
+++ Count-The-Digits/Count-The-Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter a number: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+input = Math.Abs(input);
+
+int[] digits = new int[10];
+
+while (input > 0)
+{
+    int digit = input % 10;
+    digits[digit]++;
+    input /= 10;
+}
+
+for (int i = 0; i < digits.Length; i++)
+{
+    if (digits[i] > 0)
+    {
+        Console.WriteLine($"Digit {i} occurs {digits[i]} times.");
+    }
+}
+
