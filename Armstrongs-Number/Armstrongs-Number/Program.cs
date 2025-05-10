a29a5fcc07ba798965e2e0bc9f634b5900aa1412
tamanazehra <zehratamana3@gmail.com>
2025-05-10 16:28:05 +0530

    Armstromg's-Number

---
+++ Armstrongs-Number/Armstrongs-Number/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Enter a number: ");
+int input = int.Parse(Console.ReadLine());
+
+int originalNumber = input;
+Math.Abs(input);
+
+int count = 0;
+int temp = input;
+
+while (temp > 0)
+{
+    temp /= 10;
+    count++;
+}
+
+int sum = 0;
+temp = input;
+while (temp > 0)
+{
+    int digit = temp % 10;
+    sum += (int)Math.Pow(digit, count);
+    temp /= 10;
+}
+
+if (sum == originalNumber)
+{
+    Console.WriteLine($"{originalNumber} is an Armstrong number.");
+}
+else
+{
+    Console.WriteLine($"{originalNumber} is not an Armstrong number.");
+}
+    Console.ReadLine();
+
+
+
